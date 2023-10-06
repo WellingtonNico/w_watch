8acb3d2830dbc2c8146b408f1b96b40527e89cfe
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2023-10-06 01:07:35 -0300

    feat: add funções para facilitar triggers

--- w_watch.js
+++ w_watch.js
@@ -1,10 +1,17 @@
 class WWatch {
   /**
-   * @typedef {Object} WatcherConfig
-   * @property {HTMLElement} watch - seletor css do alvo
+   * @typedef {Object} WWatcherConfig
+   * @property {Element} watch - seletor css do alvo
    * @property {string} tipo - pode ser um evento específico ou algum evento de mutação
    * @property {string} efeito - string sendo a função a ser disparada ao rodar o evento
    * @property {boolean} rodarSeco - se verdadeiro irá rodar um evento seco se possível
+   */
+  
+  /**
+   * @typedef {Object} WTriggerConfig
+   * @property {string} tipo - pode ser um evento específico ou algum evento de mutação
+   * @property {Element} dispatcher - elemento que vai disparar o evento
+   * @property {string} nomeEvento - nome do evento a ser disparado
    */
   
   atributoWWatcherConfigurado = 'w-watch-ready'
@@ -22,17 +29,27 @@ class WWatch {
   
   /**
    * @param {HTMLElement} watcher
-   * @returns {WatcherConfig[]} lista de configurações do elemento
+   * @returns {WWatcherConfig[]} lista de configurações do elemento
    */
-  obterConfiguracoesDoWatcher(watcher) {
+  obterConfiguracoesDoWWatcher(watcher) {
     const watchList = watcher.getAttribute("w-watch").split(",");
+    /** @type {WWatcherConfig[]} */
     const watchConfigList = [];
     const efeitoPadrao = watcher.getAttribute('w-effect') ?? ''
     watchList.forEach((config, index) => {
       const [seletor, tipo] = config.split(":");
       let watch;
       try {
-        watch = document.querySelector(seletor);
+        if (['document', 'window'].includes(seletor)) {
+          watch = eval(seletor)
+        } else if (seletor === 'this') {
+          watch = watcher
+        } else {
+          watch = document.querySelector(seletor);
+        }
+        if (!watch) {
+          throw new Error()
+        }
       } catch {
         console.warn(`WWatch: não foi possível localizar o alvo`);
       }
@@ -53,50 +70,124 @@ class WWatch {
   
   /**
    *
-   * @param {HTMLElement} watcher
-   * @param {WatcherConfig[]} watchConfigList
+   * @param {HTMLElement} wwatcher
+   * @param {WWatcherConfig[]} wwatcherConfigs
    */
-  adicionarWWatchEventos(watcher, watchConfigList) {
-    watchConfigList.forEach((watcherConfig) => {
-      const watch = watcherConfig.watch
-      const func = new Function(watcherConfig.efeito)
-      watcher.watch = watch
-      if (this.chavesDeMutacao.includes(watcherConfig.tipo)) {
+  adicionarWWatchEventos(wwatcher, wwatcherConfigs) {
+    wwatcherConfigs.forEach((wwatcherConfig) => {
+      const watch = wwatcherConfig.watch
+      const func = new Function(wwatcherConfig.efeito)
+      wwatcher.watch = watch
+      if (this.chavesDeMutacao.includes(wwatcherConfig.tipo)) {
         const observer = new MutationObserver((mutationsList, _) => {
           mutationsList.forEach((mutation) => {
-            if (mutation.type === watcherConfig.tipo) {
-              func.call(watcher)
+            if (mutation.type === wwatcherConfig.tipo) {
+              func.call(wwatcher)
             }
           });
         });
-        observer.observe(watcherConfig.watch, {[watcherConfig.tipo]: true});
+        observer.observe(wwatcherConfig.watch, {[wwatcherConfig.tipo]: true});
       } else {
-        watcherConfig.watch.addEventListener(watcherConfig.tipo, (event) => {
-          func.call(watcher)
+        wwatcherConfig.watch.addEventListener(wwatcherConfig.tipo, (event) => {
+          func.call(wwatcher)
         })
       }
-      if (watcherConfig.rodarSeco) {
-        func.call(watcher)
+      if (wwatcherConfig.rodarSeco) {
+        func.call(wwatcher)
       }
     });
   }
   
+  /**
+   * @param {HTMLElement} wtrigger
+   * @returns {WTriggerConfig[]}
+   */
+  obterConfiguracoesDoWTrigger(wtrigger) {
+    const triggers = wtrigger.getAttribute('w-trigger').split(',')
+    /** @type {WTriggerConfig[]} */
+    const wtriggerConfigs = []
+    triggers.forEach(trigger => {
+      let [tipo, nomeEvento, seletorDispatcher] = trigger.split(':')
+      if (!seletorDispatcher) {
+        seletorDispatcher = 'document'
+      }
+      let dispatcher
+      try {
+        if (['document', 'window'].includes(seletorDispatcher)) {
+          dispatcher = eval(seletorDispatcher)
+        } else if (seletorDispatcher === 'this') {
+          dispatcher = seletorDispatcher
+        } else {
+          dispatcher = document.querySelector(seletorDispatcher);
+        }
+        if (!dispatcher) {
+          throw new Error()
+        }
+        if (!tipo || !nomeEvento) {
+          console.warn(`WWatch: pulando configuração do wtrigger `, wtrigger);
+        } else {
+          wtriggerConfigs.push({dispatcher, nomeEvento, tipo})
+        }
+      } catch (_) {
+        console.warn(`WWatch: não foi possível localizar dispatcher`)
+      }
+    })
+    return wtriggerConfigs
+  }
   
   // TODO: add attributo para disparo de evento automático w-event-change="checkBoxAlterada" ou dinamico sendo w-event-mouseenter
   // TODO: add observer para rodar novamente a qualquer alteração na dom
   
   inicializarWWatchers() {
     [...this.obterWWatchers()].forEach(watcher => {
-      this.adicionarWWatchEventos(watcher, this.obterConfiguracoesDoWatcher(watcher))
+      this.adicionarWWatchEventos(watcher, this.obterConfiguracoesDoWWatcher(watcher))
       watcher.setAttribute(this.atributoWWatcherConfigurado, '')
     })
   }
   
-  inicializarWTriggers(){
-    const listaWTriggers = [...this.obterWTriggers()]
+  /**
+   * @param {string} nome
+   * @param {Element} target
+   * @param {Element} dispacher
+   */
+  despacharEvento(nome, target, dispacher) {
+    const wEvento = new Event(nome, {bubbles: true, cancelable: true})
+    wEvento.target = target
+    dispacher.dispatchEvent(wEvento)
+  }
+  
+  /**
+   * @param {Element} wtrigger
+   * @param {WTriggerConfig[]} wtriggerConfigs
+   */
+  adicionarWTriggerEventos(wtrigger, wtriggerConfigs) {
+    wtriggerConfigs.forEach(wtriggerConfig => {
+      if (this.chavesDeMutacao.includes(wtriggerConfig.tipo)) {
+        const observer = new MutationObserver((mutationsList, _) => {
+          mutationsList.forEach((mutation) => {
+            if (mutation.type === wtriggerConfig.tipo) {
+              this.despacharEvento(wtriggerConfig.nomeEvento, wtrigger, wtriggerConfig.dispatcher)
+            }
+          });
+        });
+        observer.observe(wtrigger, {[wtriggerConfig.tipo]: true});
+      } else {
+        wtrigger.addEventListener(wtriggerConfig.tipo, (event) => {
+          this.despacharEvento(wtriggerConfig.nomeEvento, wtrigger, wtriggerConfig.dispatcher)
+        })
+      }
+    })
+  }
+  
+  inicializarWTriggers() {
+    [...this.obterWTriggers()].forEach(wtrigger => {
+      this.adicionarWTriggerEventos(wtrigger, this.obterConfiguracoesDoWTrigger(wtrigger))
+      wtrigger.setAttribute(this.atributoWTriggerConfigurado, '')
+    })
   }
   
   inicializar() {
     this.inicializarWWatchers()
+    this.inicializarWTriggers()
   }
 }
